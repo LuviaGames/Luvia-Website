4e78869f63929760f7de449fe2fedbc69348e94a
Luvia Games <luviagames@gmail.com>
2022-01-31 21:13:28 +0100

    Dev: render privacy policy

--- app/core/ui/ui.js
+++ app/core/ui/ui.js
@@ -33,49 +33,112 @@ APP.CORE.UI = (function(){
         var gameList = document.getElementById("games-list");
         gameList.innerHTML = "";
         for(var i=0; i<games.length; i++){
-            var gameCard = document.createElement("DIV");
-            gameCard.className = "game-card";
-            var gameCover = document.createElement("IMG");
-            gameCover.className = "game-cover";
-            gameCover.src = games[i].cover;
-            var gameName = document.createElement("P");
-            gameName.className = "game-name";
-            gameName.innerText = games[i].name[lang];
+            var gameCard = _createElement({
+                tag: "DIV",
+                className: "game-card"
+            });
+            var gameCover = _createElement({
+                tag: "IMG",
+                className: "game-cover",
+                src: games[i].cover
+            });
+            var gameName = _createElement({
+                tag: "P",
+                className: "game-name",
+                textContent: games[i].name[lang]
+            });
             gameCard.append(gameCover, gameName);
             gameList.append(gameCard);
         }
-    }
+    };
 
     var renderNews = function(news){
         var lang = APP.CORE.UI.getUserLanguage();
         var newsList = document.getElementById("news-list");
         for(var i=0; i<news.length; i++){
-            var newsCard = document.createElement("DIV");
-            newsCard.className = "news-card";
-            var newDate = document.createElement("DIV");
-            newDate.className = "new-date";
-            newDate.innerHTML = news[i].date[lang];
-            var newHeader = document.createElement("DIV");
-            newHeader.className = "new-header";
-            newHeader.innerHTML = news[i].header[lang];
-            var newBrief = document.createElement("DIV");
-            newBrief.className = "new-brief";
-            newBrief.innerHTML = news[i].brief[lang];
-            var newDescription = document.createElement("DIV");
-            newDescription.className = "new-description";
-            newDescription.innerHTML = news[i].description[lang];
+            var newsCard = _createElement({
+                tag: "DIV",
+                className: "news-card"
+            });
+            var newDate = _createElement({
+                tag: "DIV",
+                className: "new-date",
+                innerHTML: news[i].date[lang]
+            });
+            var newHeader = _createElement({
+                tag: "DIV",
+                className: "new-header",
+                innerHTML: news[i].header[lang]
+            });
+            var newBrief = _createElement({
+                tag: "DIV",
+                className: "new-brief",
+                innerHTML: news[i].brief[lang]
+            });
+            var newDescription = _createElement({
+                tag: "DIV",
+                className: "new-description",
+                innerHTML: news[i].description[lang]
+            });
             newsCard.append(newDate, newHeader, newBrief, newDescription);
             newsList.append(newsCard);
         }
-    }
+    };
 
     var renderPrivacy = function(privacy){
         var lang = APP.CORE.UI.getUserLanguage();
         var privacyPolicy = document.getElementById("privacy-policy");
         for(var i=0; i<privacy.length; i++){
-            console.log(privacy[i]);
+            var privacyBlock = _createElement({
+                tag: "DIV",
+                className: "privacy-" + privacy.type
+            });
+            if(Array.isArray(privacy.text[lang])){
+                for(var j=0; j<privacy.text[lang].length; j++){
+                    var text = privacy.text[lang][j];
+                    var privacyText = _createElement({
+                        tag: "P",
+                        className: "",
+                        innerHTML: text
+                    });
+                    privacyBlock.append(privacyText);
+                }
+            }
+            else {
+                var privacyText = _createElement({
+                    tag: "P",
+                    className: "",
+                    innerHTML: privacy.text[lang]
+                });
+                privacyBlock.append(privacyText);
+            }
+            privacyPolicy.append(privacyBlock);
         }
-    }    
+    };
+
+    /**
+     * CREATE FRAGMENT
+     * @returns {DOM Element}
+     */
+     var _createFragment = function(){
+        return new DocumentFragment();
+    };    
+
+    /**
+     * CREATE ELEMENT -  Creates a DOM Element with attributes
+     * @param {json} attributes
+     * @returns {DOM Element} element
+     */
+    var _createElement = function(attributes){
+        var element = document.createElement(attributes.tag);
+        for(attr in attributes){
+            if(attr.includes("data")){
+                element.setAttribute(attr.replace("data", "data-").toLowerCase(), attributes[attr]);
+            }
+            element[attr] = attributes[attr];
+        }
+        return element;
+    };
 
     return {
         getUserLanguage: getUserLanguage,
